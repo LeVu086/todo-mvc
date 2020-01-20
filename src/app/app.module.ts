6d527ca863212d7adea2dd27a174a2166f8d8930
vule <levu086it@gmail.com>
2020-01-20 16:38:57 +0700

    add service components model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { TodoInputComponent } from './components/todo-input/todo-input.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoItemComponent,
+    TodoInputComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
